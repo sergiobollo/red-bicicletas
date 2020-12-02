e8e5fcfede142f1a384eab99836b5f639774fde5
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-12-02 04:19:58 -0300

    Creacion de API

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,6 +4,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href= "https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-34.60548328404713, -58.493011531268195]).addTo(map);
-L.marker([-34.603332593746835, -58.498896460139385]).addTo(map);
-L.marker([-34.60524005501132, -58.48670850337339]).addTo(map);
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        })
+    }
+})
