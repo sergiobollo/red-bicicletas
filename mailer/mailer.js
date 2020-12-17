0d5d0e0066e208c5740c1006d548adec830d8582
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-12-16 23:31:51 -0300

    se agrego sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,13 +1,37 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
+let mailConfig;
+if (process.env.NODE_ENV === 'production'){
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
+    }
+    mailConfig = sgTransport(options);
 
-const mailConfig = {
+} else {
+    if (process.env.NODE_ENV === 'staging'){
+        console.log('XXXXXXXXXX');
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(options);
+
+    } else {
+        // all emails are catched by ethereal.email
+        mailConfig = {
             host: 'smtp.ethereal.email',
             port: 587,
+            secure: false,
             auth: {
-                user: 'kelton.corkery79@ethereal.email',
-                pass: 'GED153KmQuKG5ArWtz'
+                user: process.env.ethereal_user,
+                pass: process.env.ethereal_pwd
             }
         };
+    }
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
