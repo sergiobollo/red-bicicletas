3fc0f50adeb80434b3a67dd24622db0ce5fb7fc8
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2020-12-18 03:54:23 -0300

    se agrego auth facebook

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,5 +5,6 @@ const authController = require('../../controllers/api/authControllerAPI');
 
 router.post('/authenticate', authController.authenticate);
 router.post('/forgotPassword', authController.forgotPassword);
+router.post('/facebook_token', passport.authenticate('facebook-token'), authController.authFacebookToken);
 
 module.exports = router;
